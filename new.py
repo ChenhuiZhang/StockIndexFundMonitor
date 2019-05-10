307ca046977b23a65a29e43191f201ef8e13061a
Hermes Zhang <chenhuiz@axis.com>
2019-05-10 16:52:47 +0800

    WIP
    Change-Id: If354e554a90aebce096d7e3e81e2f5f64de31124

--- new.py
+++ new.py
@@ -24,7 +24,7 @@ def save_csv(code, duration):
     begin = now - datetime.timedelta(days=duration)
     fields = "TCLOSE;TOPEN;LCLOSE;CHG;PCHG"
 
-    csv = "%s_%s.csv" % (code, begin.strftime("%Y%m%d"))
+    csv = "%s_%s_%d.csv" % (code, begin.strftime("%Y%m%d"), duration)
     url = 'http://quotes.money.163.com/service/chddata.html?code=%s&start=%s&end=%s&fields=%s' \
             % (code, begin.strftime("%Y%m%d"), now.strftime("%Y%m%d"), fields)
     print url
@@ -43,12 +43,50 @@ def plot_csv(csv):
     df = df.reset_index(drop=True)
     print df.head(5)
 
+    print len(df.index)
+    rows = len(df.index)
+
+    print df.tail(30)
+    print df.tail(30)[u'收盘价']
+    print df.tail(60)[u'收盘价'].mean()
+    avg30 = df.tail(90)[u'收盘价'].mean()
+    #print df.ix[-30:, u'收盘价']
+    #print df.ix[-30:, u'收盘价'].mean()
+    #avg30 = df.ix[-30:, u'收盘价'].mean()
+
     ax = df.plot(x=u'日期', y=[u'收盘价'])
     ax.set_xticks(df.index)
     ax.set_xticklabels(df[u'日期'], rotation=60)
+    #df.plot.line(x=u'日期', avg30)
+
+    s = float(30)/rows
+    print s
+    if rows > 30:
+        avg30 = df.tail(30)[u'收盘价'].mean()
+        start = 1 - float(30)/rows
+        plt.axhline(y=avg30, xmin=start, xmax=1, color='r', linestyle='-', label=u'30日均线')
+    if rows > 60:
+        avg60 = df.tail(60)[u'收盘价'].mean()
+        start = 1 - float(60)/rows
+        plt.axhline(y=avg60, xmin=start, xmax=1, color='m', linestyle='-', label=u'60日均线')
+    if rows > 90:
+        avg90 = df.tail(90)[u'收盘价'].mean()
+        start = 1 - float(90)/rows
+        plt.axhline(y=avg90, xmin=start, xmax=1, color='c', linestyle='-', label=u'90日均线')
+    if rows > 180:
+        avg180 = df.tail(180)[u'收盘价'].mean()
+        start = 1 - float(180)/rows
+        plt.axhline(y=avg180, xmin=start, xmax=1, color='g', linestyle='-', label=u'180日均线')
+
+    plt.legend()
+
     plt.show()
 
 if __name__ == '__main__':
-    csv_file = save_csv("0000300", 180)
+    #csv_file = save_csv("0000016", 180)
+    #plot_csv(csv_file)
 
+    csv_file = save_csv("0000300", 300)
     plot_csv(csv_file)
+    #plot_csv("0000300_20181110_180.csv")
+
