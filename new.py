5d5f2cc916ef36a3d02820513b791cdc33a612d1
Hermes Zhang <chenhuiz@axis.com>
2019-05-24 12:45:21 +0800

    up
    Change-Id: I55085adaa2f2a1befd93a83a72754f4495ed958c

--- new.py
+++ new.py
@@ -2,6 +2,7 @@
 import io
 import datetime
 import requests
+import re
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -64,11 +65,38 @@ def plot_csv(csv):
 
     plt.show()
 
+def xxxx(name):
+    url = "http://hq.sinajs.cn/list=%s" % name
+
+    html = get_data(url)
+
+    print html
+
+    pattern = re.compile(r'\d+\.\d+')
+
+    match = re.findall(pattern, html)
+
+    today = match[0]
+    diff = match[1]
+    percentage = match[2]
+
+    print today, diff, percentage
+
+    if percentage < -0.04:
+        print "Do"
+    elif percentage < -0.02:
+        print "Do and check"
+    else:
+        print "Nothing"
+
+    
+
 if __name__ == '__main__':
     #csv_file = save_csv("0000016", 180)
     #plot_csv(csv_file)
 
-    csv_file = save_csv("0000300", 150)
-    plot_csv(csv_file)
+    #csv_file = save_csv("0000300", 30)
+    #plot_csv(csv_file)
     #plot_csv("0000300_20181110_180.csv")
 
+    xxxx("s_sh000300")
